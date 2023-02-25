1cb33f6b07005cd1925ba9c862df1e02e5199d4e
DiabloZ <Diabloz@me.com>
2023-02-25 13:25:34 +0300

    Created Previews

---
+++ app/src/main/java/com/suhov/aaspc2023/ui/components/Previews.kt
@@ -0,0 +1,31 @@
+package com.suhov.aaspc2023.ui.components
+
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+	name = "Phone",
+	group = "Devices",
+	showSystemUi = true,
+	device = Devices.PIXEL
+)
+annotation class PhonePreview
+
+@Preview(
+	name = "Tablet",
+	group = "Devices",
+	showSystemUi = true,
+	device = Devices.TABLET
+)
+annotation class TabletPreview
+
+@PhonePreview
+@TabletPreview
+annotation class DevicesPreview
+
+@Preview(
+	name = "Element",
+	group = "Element",
+	showSystemUi = false,
+)
+annotation class ElementPreview
