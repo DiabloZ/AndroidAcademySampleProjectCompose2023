59317b5979b209c6d77c78f6041f165259234ec2
DiabloZ <Diabloz@me.com>
2023-02-25 13:23:21 +0300

    Changed way of get libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "com.suhov.aaspc2023"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
@@ -47,19 +47,29 @@ android {
 }
 
 dependencies {
-    val compose_version = extra.properties["compose_version"]
 
-    implementation ("androidx.core:core-ktx:1.9.0")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation ("androidx.activity:activity-compose:1.6.1")
-    implementation ("androidx.compose.ui:ui:${compose_version}")
-    implementation ("androidx.compose.ui:ui-tooling-preview:$compose_version")
-    implementation ("androidx.compose.material3:material3:1.1.0-alpha07")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0-rc01")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:$compose_version")
-    debugImplementation ("androidx.compose.ui:ui-tooling:$compose_version")
-    debugImplementation ("androidx.compose.ui:ui-test-manifest:$compose_version")
+    implementation (libs.ktx.core)
+    implementation (libs.ktx.runtime)
+    implementation (libs.ktx.viewmodel)
+    implementation (libs.compose.activity)
+    implementation (libs.compose.preview)
+    implementation (libs.compose.material)
+    implementation (libs.compose.material3)
+    implementation (libs.compose.materialIcons)
+    debugImplementation (libs.compose.uiTooling)
+
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.coil)
+
+    implementation(libs.timber)
+
+    /*implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)*/
+
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+
+    implementation(libs.okhttp.loggingInterceptor)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+/*    dependencies {
+        classpath(libs.androidGradle)
+        classpath(libs.kotlinGradle)
+        classpath(libs.hiltGradle)
+        classpath(libs.googleServicesGradle)
+    }*/
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
