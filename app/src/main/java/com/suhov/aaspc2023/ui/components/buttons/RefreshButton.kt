827b48f5ab68b4d42b0ef60c118d57aa9476f3c9
DiabloZ <Diabloz@me.com>
2023-02-26 17:52:06 +0300

    Created rest of part error screen

--- app/src/main/java/com/suhov/aaspc2023/ui/components/buttons/RefreshButton.kt
+++ app/src/main/java/com/suhov/aaspc2023/ui/components/buttons/RefreshButton.kt
@@ -5,10 +5,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,7 +22,7 @@ import com.suhov.aaspc2023.ui.components.previews.ElementPreview
 @Composable
 fun RefreshButtonPreview(){
 	RefreshButton(
-		text = "Some button",
+		text = "Some button Some button Some button Some button Some button Some button",
 		onClick = {},
 		leftIconRes = R.drawable.ic_refresh,
 		leftIconDescription = null,
@@ -47,26 +44,29 @@ fun RefreshButton(
 	leftIconDescription: String? = null,
 	@DrawableRes rightIconRes: Int? = null,
 	rightIconDescription: String? = null,
+	shape: Dp = 8.dp,
 	spacerValue: Dp = 8.dp,
+	contentPadding:Dp = 16.dp,
 	contentColor: Color = Color.Yellow,
 	backgroundColor: Color = Color.Cyan
 ){
 	Button(
 		onClick = onClick,
 		modifier = modifier,
-		shape = RoundedCornerShape(topStart = 8.dp, bottomEnd = 8.dp),
-		contentPadding = PaddingValues(8.dp),
+		shape = RoundedCornerShape(shape),
+		contentPadding = PaddingValues(contentPadding),
 		colors = ButtonDefaults.buttonColors(
 			backgroundColor = backgroundColor,
 			contentColor = contentColor
-		)
+		),
 	) {
 		LeftIcon(leftIconRes, leftIconDescription, spacerValue)
 		Text(
 			text = text,
 			style = MaterialTheme.typography.headlineSmall,
-			textAlign = TextAlign.Center,
+			textAlign = TextAlign.Start,
 			fontWeight = FontWeight.Bold,
+			modifier = Modifier.weight(1f)
 		)
 		RightIcon(rightIconRes, rightIconDescription, spacerValue)
 	}
---
+++ app/src/main/java/com/suhov/aaspc2023/ui/components/screens/errorloaddata/parts/ContentErrorScreen.kt
@@ -0,0 +1,59 @@
+package com.suhov.aaspc2023.ui.components.screens.errorloaddata.parts
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.suhov.aaspc2023.ui.components.ClickableSmallText
+import com.suhov.aaspc2023.ui.components.ErrorCodeText
+import com.suhov.aaspc2023.ui.components.ErrorImage
+import com.suhov.aaspc2023.ui.components.ErrorWarningText
+import com.suhov.aaspc2023.ui.components.previews.ElementPreview
+import com.suhov.aaspc2023.ui.components.previews.PhonePreview
+
+@ElementPreview
+@Composable
+private fun ContentErrorScreenPreviewElement() {
+	ContentErrorScreen(
+		onLinkClick = {},
+		Modifier.padding(16.dp),
+	)
+}
+
+@PhonePreview
+@Composable
+private fun ContentErrorScreenPreviewPhone() {
+	ContentErrorScreen(
+		onLinkClick = {},
+	)
+}
+
+@Composable
+fun ContentErrorScreen(
+	onLinkClick: (link: String) -> Unit,
+	modifier: Modifier = Modifier,
+) {
+	Column(
+		modifier,
+		verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+		horizontalAlignment = Alignment.CenterHorizontally,
+		//content = {}
+	) {
+		ErrorImage( link = "https://www.pngaaa.com/api-download/3018884" )
+		ErrorWarningText(text = "Whoops, we couldn't load the data!")
+		ErrorCodeText(
+			text = "Error code",
+			errorCode = "BB-404"
+		)
+		ClickableSmallText(
+			text = "Something went wrong,\n",
+			clickableText = "please tap on this link and we try to fix it.",
+			linkForClick = "https://www.google.com",
+			onTextClick = onLinkClick
+		)
+	}
+
+}
