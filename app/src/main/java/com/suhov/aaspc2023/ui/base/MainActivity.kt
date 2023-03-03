88acae3b270ce98a54a4b0c8822d76910c8ca200
Vitaly Sukhov <sukhov_v@tanukitech.com>
2023-03-03 22:33:42 +0300

    Moved Activity

--- app/src/main/java/com/suhov/aaspc2023/ui/base/MainActivity.kt
+++ app/src/main/java/com/suhov/aaspc2023/ui/base/MainActivity.kt
@@ -1,31 +1,56 @@
-package com.suhov.aaspc2023
+package com.suhov.aaspc2023.ui.base
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.animation.core.repeatable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.suhov.aaspc2023.data.services.ApiGitHubService
 import com.suhov.aaspc2023.ui.components.previews.PhonePreview
 import com.suhov.aaspc2023.ui.components.screens.errorloaddata.ErrorScreen
 import com.suhov.aaspc2023.ui.theme.AndroidAcademySampleProjectCompose2023Theme
 import com.suhov.aaspc2023.ui.util.navigation.Screen
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class MainActivity : ComponentActivity() {
+
+	private val viewModel: MainActivityViewModel by viewModels()
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
+
+		lifecycleScope.launch {
+			repeatOnLifecycle(Lifecycle.State.STARTED){
+				viewModel.getRepos()
+				viewModel.gitHubRepoData.collect { repos ->
+					Timber.e("MAIN ACTIVITY TEST - $repos")
+				}
+			}
+		}
 		setContent {
 			AndroidAcademySampleProjectCompose2023Theme {
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
+/*					val testViewModel: MainActivityViewModel = viewModel()
+					testViewModel.getRepos()*/
 					val navController = rememberNavController()
 					navController.addOnDestinationChangedListener{
 							controller,
